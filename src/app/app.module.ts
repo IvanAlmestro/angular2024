ba48eed6ef628eef1a5482833d6af69fd46aa5f7
Ivan <ivanalmestro@hotmail.com>
2024-07-09 18:25:02 -0300

    primeros componentes y creacion de la lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClothListComponent } from './cloth-list/cloth-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClothListComponent
   ],
   imports: [
     BrowserModule,
